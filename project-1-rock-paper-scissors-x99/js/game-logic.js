b2f6b9a2fabfcaf2a3fd3da579defef306ed67a3
Anson Felder <afelder@Ansons-MacBook-Pro.local>
2017-10-27 11:42:02 -0400

    Finished RPS Project

--- project-1-rock-paper-scissors-x99/js/game-logic.js
+++ project-1-rock-paper-scissors-x99/js/game-logic.js
@@ -1,5 +1,5 @@
 // All code should be written in this file.
-
+console.log("Running program")
 // set 12 global variables
 let playerOneMoveOneType;
 let playerOneMoveOneValue;
@@ -16,26 +16,21 @@ let playerTwoMoveThreeType;
 let playerTwoMoveThreeValue;
 
 // Function to set
-const setPlayerMoves = (player,
-                        moveOneType,
-                        moveTwoType,
-                        moveThreeType,
-                        moveOneValue,
-                        moveTwoValue,
-                        moveThreeValue
-                      ) => {
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
   //check input values
+  // console.log(player,moveOneType,moveOneValue,moveTwoType,moveOneValue,moveThreeType,moveThreeValue)
   if(
-    isValidPlayerName(player) &&
-    isValidMoveType( moveOneType ) &&
-    isValidMoveType( moveTwoType ) &&
-    isValidMoveType( moveThreeValue) &&
-    isValidMoveValue( moveOneValue ) &&
-    isValidMoveValue( moveTwoValue ) &&
-    isValidMoveValue( moveThreeValue )
+    isValidPlayerName(player)
+    && isValidMoveType( moveOneType )
+    && isValidMoveType( moveTwoType )
+    && isValidMoveType( moveThreeType)
+    && isValidMoveValue( moveOneValue )
+    && isValidMoveValue( moveTwoValue )
+    && isValidMoveValue( moveThreeValue )
+    && isValidMoveValues( moveOneValue, moveTwoValue, moveThreeValue )
   ){
     //conduct comparison if all values are valid
-    if(player == 'Player One'){
+    if(player === 'Player One'){
       // console.log('In the player one if statement')
       playerOneMoveOneType = moveOneType;
       // console.log('Player one move one type:', playerOneMoveOneType)
@@ -44,80 +39,118 @@ const setPlayerMoves = (player,
       playerOneMoveTwoValue = moveTwoValue;
       playerOneMoveThreeType = moveThreeType;
       playerOneMoveThreeValue = moveThreeValue;
-    } else if(player == 'Player Two') {
-      playerTwoMoveOneType = moveOneValue;
+    } else if(player === 'Player Two') {
+      playerTwoMoveOneType = moveOneType;
       playerTwoMoveOneValue = moveOneValue;
       playerTwoMoveTwoType = moveTwoType;
       playerTwoMoveTwoValue = moveTwoValue;
-      playerOneMoveThreeType = moveThreeType;
+      playerTwoMoveThreeType = moveThreeType;
       playerTwoMoveThreeValue = moveThreeValue;
     }
+  }
+  else {
+    //console.log("Invalid inputs to setPlayerMoves function")
   }
  };
 
 const getRoundWinner = (roundNumber) => {
+  if( isValidRound( roundNumber ) && isValidRoundVariables(roundNumber) ){
 
-  if(roundNumber === 1){
-    return compareTypes(playerOneMoveOneType,playerTwoMoveOneType);
+    let result;
+    if(roundNumber === 1){
+      // console.log('_______Round One_________')
+      // console.log('Player One Move: ',playerOneMoveOneType, ' ', playerOneMoveOneValue)
+      // console.log('Player Two Move: ',playerTwoMoveOneType, ' ', playerTwoMoveOneValue)
+      result = compareTypes(playerOneMoveOneType,playerTwoMoveOneType);
+      if(result === 'Tie'){
+        result = tieBreak(playerOneMoveOneValue,playerTwoMoveOneValue);
+      }
+      return result;
+    }
+    else if(roundNumber === 2){
+      // console.log('_______Round Two_________')
+      // console.log('Player One Move: ',playerOneMoveTwoType, ' ', playerOneMoveTwoValue)
+      // console.log('Player Two Move: ',playerTwoMoveTwoType, ' ', playerTwoMoveTwoValue)
+      result = compareTypes(playerOneMoveTwoType,playerTwoMoveTwoType);
+      if(result === 'Tie'){
+        result = tieBreak(playerOneMoveTwoValue,playerTwoMoveTwoValue);
+      }
+      return result;
+    }
+    else if(roundNumber === 3){
+      // console.log('_______Round Three_________')
+      // console.log('Player One Move: ',playerOneMoveThreeType, ' ', playerOneMoveThreeValue)
+      // console.log('Player Two Move: ',playerTwoMoveThreeType, ' ', playerTwoMoveThreeValue)
+      result = compareTypes(playerOneMoveThreeType,playerTwoMoveThreeType);
+      if(result === 'Tie'){
+        result = tieBreak(playerOneMoveThreeValue,playerTwoMoveThreeValue);
+      }
+      return result;
+    }
   }
-  else if(roundNumber === 2){
-    return compareTypes(playerOneMoveTwoType,playerTwoMoveTwoType);
-  }
-  else if(roundNumber === 3){
-    return compareTypes(playerOneMoveThreeType,playerTwoMoveTwoType);
-  }
-  else{
-    return null;
-  }
+  else {
+    // console.log('Invalid round')
+    return null;}
 }
-
 
 const getGameWinner = () => {
   //check if values are null
+  if( isValidGlobalVariables() ){
+    //set local variables
+    let playerOneWins = 0;
+    let playerTwoWins = 0;
 
+    //get round winners
+    let roundOneWinner = getRoundWinner(1);
+    let roundTwoWinner = getRoundWinner(2);
+    let roundThreeWinner = getRoundWinner(3);
 
-  //set local variables
-  let playerOneWins = 0;
-  let playerTwoWins = 0;
+    // console.log(':::::::::::::RESULTS:::::::::::')
+    // console.log('Round One Winner: ', roundOneWinner)
+    // console.log('Round Two Winner: ', roundTwoWinner)
+    // console.log('Round Three Winner: ', roundThreeWinner)
 
-  //Round One
-  let roundOneWinner = getRoundWinner(1)
-  if(roundOneWinner === 'Player One'){playerOneWins++}
-  else if(roundOneWinner === 'Player Two'){playerTwoWins++}
-  else{
-    if(playerOneMoveOneValue>playerTwoMoveOneValue){playerOneWins++}
-    else if(playerOneMoveValue<playerTwoMoveOneValue){playerTwoWins++}
+    if(roundOneWinner === 'Player One'){playerOneWins++}
+    else if(roundOneWinner === 'Player Two'){playerTwoWins++}
+
+    if( roundTwoWinner === 'Player One'){playerOneWins++}
+    else if(roundTwoWinner === 'Player Two'){playerTwoWins++}
+
+    if( roundThreeWinner === 'Player One'){playerOneWins++}
+    else if(roundThreeWinner === 'Player Two'){playerTwoWins++}
+
+    // console.log('Player One Points: ', playerOneWins)
+    // console.log('Player Two Points: ', playerTwoWins)
+    if( playerOneWins === playerTwoWins){return 'Tie'}
+    else if( playerOneWins > playerTwoWins){return 'Player One'}
+    else if( playerOneWins < playerTwoWins){return 'Player Two'}
+    else { return null }
+
   }
-
-  //Round Two
-  let roundTwoWinner = getRoundWinner(2)
-  if(roundTwoWinner === 'Player One'){playerOneWins++}
-  else if(roundTwoWinner === 'Player Two'){playerTwoWins++}
-  else{
-    if(playerOneMoveTwoValue>playerTwoMoveTwoValue){playerOneWins++}
-    else if(playerOneMoveTwoValue<playerTwoMoveTwoValue){playerTwoWins++}
-  }
-
-  //Round Three
-  let roundThreeWinner = getRoundWinner(3)
-  if(roundThreeWinner === 'Player One'){playerOneWins++}
-  else if(roundThreeWinner === 'Player Two'){playerTwoWins++}
-  else{
-    if(playerOneMoveThreeValue>playerTwoMoveThreeValue){playerOneWins++}
-    else if(playerOneMoveThreeValue<playerTwoMoveThreeValue){playerTwoWins++}
-  }
-
-  //Determine winner
-  if(playerOneWins > playerTwoWins){return 'Player One'}
-  else if(playerOneWins < playerTwoWins){return 'Player Two'}
-  else if (playerOneWins === playerTwoWins){return 'Tie'}
   else {return null}
 }
 
 const setComputerMoves = () => {
   //set moves
+  playerTwoMoveOneValue = getRandomNumber(97)
+  playerTwoMoveTwoValue = getRandomNumber(98 - playerTwoMoveOneValue)
+  playerTwoMoveThreeValue = 99 - playerTwoMoveOneValue - playerTwoMoveTwoValue
 
   //set values
+  playerTwoMoveOneType = getRandomMove();
+  playerTwoMoveTwoType = getRandomMove();
+  playerTwoMoveThreeType = getRandomMove();
+}
+
+const getRandomNumber = (ceiling) => {
+  return Math.ceil(Math.random()*ceiling)
+}
+
+const getRandomMove = () => {
+  let num = getRandomNumber(3)
+  if(num === 3){return 'rock'}
+  else if(num === 2){return 'scissors'}
+  else if(num === 1){return 'paper'}
 }
 
 const compareTypes = (playerOneType, playerTwoType) => {
@@ -140,21 +173,68 @@ const compareTypes = (playerOneType, playerTwoType) => {
   }
 }
 
+const tieBreak = (valueOne,valueTwo) => {
+  if(valueOne > valueTwo){
+    return 'Player One';
+  }
+  else if( valueOne < valueTwo){
+    return 'Player Two';
+  }else{
+    return 'Tie';
+  }
+}
+
 const isValidMoveType = (moveType) => {
-  if(moveType == 'rock' || moveType == 'paper' || moveType == 'scissors'){
+  if(moveType === 'rock' || moveType === 'paper' || moveType === 'scissors'){
     return true;
   }
   else {return false;}
 }
 
 const isValidPlayerName = (playerName) => {
-  if(playerName == 'Player One' || playerName == 'Player Two'){
+  if(playerName === 'Player One' || playerName === 'Player Two'){
     return true;
   }
   else {return false;}
 }
 
 const isValidMoveValue = (value) => {
-  if(value>0){return true}
+  if(value > 0 && value < 99 ){return true}
   else{return false}
+}
+
+const isValidMoveValues = (valueOne, valueTwo, valueThree) => {
+  let sum = valueOne + valueTwo + valueThree;
+  //console.log(sum)
+  if( sum > 99){
+    return false;
+  }
+  else {return true;}
+}
+
+const isValidRoundVariables = ( round ) => {
+  if(round === 1){
+    if(playerOneMoveOneType && playerOneMoveOneValue && playerTwoMoveOneType && playerTwoMoveOneValue ){
+      return true;
+    } else {return false;}
+  } else if(round === 2){
+    if(playerOneMoveTwoType && playerOneMoveTwoValue && playerTwoMoveTwoType && playerTwoMoveTwoValue ){
+      return true;
+    } else {return false;}
+  } else if(round === 3 ){
+    if(playerOneMoveThreeType && playerOneMoveThreeValue && playerTwoMoveThreeType && playerTwoMoveThreeValue){
+      return true;
+    } else {return false;}
+  }
+}
+
+const isValidGlobalVariables = () => {
+  return isValidRoundVariables(1) && isValidRoundVariables(2) && isValidRoundVariables(3)
+}
+
+const isValidRound = ( round ) => {
+  if( round === 1 || round === 2 || round === 3){
+    return true;
+  }
+  else {return false;}
 }
