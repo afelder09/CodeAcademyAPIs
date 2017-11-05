7891b8484bbd69bd8aad2d2e8b26ffd2b7ca66f1
Anson Felder <afelder@android-f2f773ad948fcd1f.localdomain>
2017-11-05 10:05:36 -0500

    First attempt at project 2

--- project-2-beat-mix/presetHandler.js
+++ project-2-beat-mix/presetHandler.js
@@ -2,7 +2,50 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (requestType, presetIndex, newPresetArray ) => {
+  // set return variable
+  // console.log('Preset length: ', presets.length)
+  let result = [];
+
+  if( isValidPreset(presetIndex) ){
+    result.push('200')
+  } else {
+    result.push('404')
+  }
+
+  if( isValidType(requestType)){
+    if( requestType == 'GET'){
+      result.push(presets[preset])
+    }
+    if( requestType == 'PUT'){
+      presets[preset] = newPresetArray;
+      result.push(presets[preset]);
+    }
+  } else {
+    result[0] = '400';
+  }
+
+  return result;
+};
+
+const isValidType = (requestType) => {
+  if(requestType == 'GET' || requestType == 'PUT'){
+    return true;
+  }
+  else{
+    console.log('Invalid type');
+    return false;
+  }
+}
+
+const isValidPreset = (presetIndex) => {
+  console.log('Preset index is: ', presetIndex )
+  if(presetIndex >= 0 && presetIndex <= presets.length-1){
+    return true;
+  } else{
+    return false;
+  }
+}
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- project-2-beat-mix/public/js/script.js
+++ project-2-beat-mix/public/js/script.js
@@ -1,6 +1,84 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
+// Toggle drum function
+const toggleDrum = ( drum, index ) => {
+  if(isValidDrum(drum) && isValidIndex(index)){
+    switch(drum) {
+      case 'kicks':
+        kicks[index] = !kicks[index];
+        break;
+      case 'snares':
+        snares[index] = !snares[index];
+        break;
+      case 'hiHats':
+        hiHats[index] = !hiHats[index];
+        break;
+      case 'rideCymbals':
+        rideCymbals[index] = !rideCymbals[index];
+        break;
+    }
+  }
+}
+
+// clear function
+const clear = ( drum ) => {
+  switch(drum) {
+    case 'kicks':
+      kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+      break;
+    case 'snares':
+      snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+      break;
+    case 'hiHats':
+      hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+      break;
+    case 'rideCymbals':
+      rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+      break;
+  }
+}
+
+const invert = ( arrayName ) => {
+  switch( arrayName ){
+    case 'kicks':
+      for(let i=0;i<kicks.length;i++){
+        kicks[i]= !kicks[i];
+      }
+      break;
+    case 'snares':
+      for(let i=0;i<snares.length;i++){
+        snares[i]= !snares[i];
+      }
+      break;
+    case 'hiHats':
+      for(let i=0;i<hiHats.length;i++){
+        hiHats[i]= !hiHats[i];
+      }
+      break;
+    case 'rideCymbals':
+      for(let i=0;i<rideCymbals.length;i++){
+        rideCymbals[i]= !rideCymbals[i];
+      }
+      break;
+  }
+}
+
+const isValidDrum = ( drum ) => {
+    if( drum == 'kicks' || drum == 'snares' || drum == 'hiHats' || drum == 'rideCymbals'){
+      return true;
+    } else {
+      return false;
+    }
+}
+
+const isValidIndex = ( index ) => {
+  if( index >=1 && index <= 16){
+    return true;
+  } else{
+    return false;
+  }
+}
